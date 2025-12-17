4fd779f173bf0c6d7f3d546864a813b01eeac399
bala-amypo <bala@amypo.in>
2025-12-17 06:36:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/Stuentity.java
+++ src/main/java/com/example/demo/entity/Stuentity.java
@@ -1,8 +1,8 @@
 package com.example.demo.entity;
 
-import jarkata.persistence.Entity;
-import jarkata.persistence.GeneratedValue;
-import jarkata.persistence.Id;
+import jakata.persistence.Entity;
+import jakata.persistence.GeneratedValue;
+import jakata.persistence.Id;
 
 @Entity
 public class Stuentity{
