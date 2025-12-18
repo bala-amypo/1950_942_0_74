1e478bafefd55f9c8712a7d0bc059885554f0081
bala-amypo <bala@amypo.in>
2025-12-18 06:27:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Stuentity.java
+++ src/main/java/com/example/demo/entity/Stuentity.java
@@ -11,44 +11,44 @@ import jakarta.persistence.Id;
 @Entity
 public class Stuentity{
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String email;
-    private float cgpa;
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
+            private Long id;
+                private String name;
+                    private String email;
+                        private float cgpa;
 
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-     public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email ){
-        this.email=email;
-    }
-     public float getcgpa(){
-        return cgpa;
-    }
-    public void setcgpa(float cgpa){
-        this.cgpa=cgpa;
-    }
-    public Stuentity(Long id, String name,String email, float cgpa){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.cgpa=cgpa;
+                            public Long getId(){
+                                    return id;
+                                        }
+                                            public void setId(Long id){
+                                                    this.id=id;
+                                                        }
+                                                            public String getName(){
+                                                                    return name;
+                                                                        }
+                                                                            public void setName(String name){
+                                                                                    this.name=name;
+                                                                                        }
+                                                                                             public String getEmail(){
+                                                                                                     return email;
+                                                                                                         }
+                                                                                                             public void setEmail(String email ){
+                                                                                                                     this.email=email;
+                                                                                                                         }
+                                                                                                                              public float getcgpa(){
+                                                                                                                                      return cgpa;
+                                                                                                                                          }
+                                                                                                                                              public void setcgpa(float cgpa){
+                                                                                                                                                      this.cgpa=cgpa;
+                                                                                                                                                          }
+                                                                                                                                                              public Stuentity(Long id, String name,String email, float cgpa){
+                                                                                                                                                                      this.id=id;
+                                                                                                                                                                              this.name=name;
+                                                                                                                                                                                      this.email=email;
+                                                                                                                                                                                              this.cgpa=cgpa;
 
-    }
-    public Stuentity(){
-        
-    }
-}
+                                                                                                                                                                                                  }
+                                                                                                                                                                                                      public Stuentity(){
+                                                                                                                                                                                                              
+                                                                                                                                                                                                                  }
+                                                                                                                                                                                                                  }
